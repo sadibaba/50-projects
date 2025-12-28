994a43065545f3cc24ca29456ddd70641650c992
saad-code <sadisheikh169@gmail.com>
2025-12-28 11:55:56 +0500

    quiz web done

--- Quiz-web/index.js
+++ Quiz-web/index.js
@@ -1,7 +1,61 @@
-const question = document.querySelector('.Question')
-const options = document.querySelector('.options')
-const option1 = document.querySelector('.option1')
-const option2 = document.querySelector('.option2')
-const option3 = document.querySelector('.option3')
-const option4 = document.querySelector('.option4')
-const counter = document.querySelector('.counter')
+const questionEl = document.querySelector('.Question');
+const optionButtons = document.querySelectorAll('.options button');
+const counterEl = document.querySelector('.counter');
+
+let questions = [];
+let currentIndex = 0;
+
+async function loadQuestion() {
+  const res = await fetch('https://opentdb.com/api.php?amount=50&category=15&type=multiple');
+  const data = await res.json();
+  questions = data.results;
+  showQuestions();
+}
+
+function showQuestions() {
+  const currentQ = questions[currentIndex];
+
+  // Question text
+  questionEl.textContent = currentQ.question;
+
+  // Mix correct + incorrect answers
+  const answers = [...currentQ.incorrect_answers, currentQ.correct_answer];
+  shuffleArray(answers);
+
+  // Render options
+  optionButtons.forEach((btn, i) => {
+    btn.textContent = answers[i];
+    btn.onclick = () => checkAnswer(answers[i], currentQ.correct_answer);
+  });
+
+  // Counter update
+  counterEl.textContent = `${currentIndex + 1}/${questions.length}`;
+}
+
+function checkAnswer(selected, correctAnswer) {
+  if (selected === correctAnswer) {
+    alert('Corrected buddy!');
+    nextQuestion();
+  } else {
+    alert('ops sorry buddy');
+  }
+}
+
+function nextQuestion() {
+  currentIndex++;
+  if (currentIndex < questions.length) {
+    showQuestions();
+  } else {
+    questionEl.textContent = 'Quiz Finished!';
+    optionButtons.forEach(btn => btn.style.display = 'none');
+  }
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+loadQuestion();
