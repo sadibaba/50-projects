d54e6f02e4d625590f828efccc9cda6c28d640d0
Sheikh M. Aizaz (Saad) <sadisheikh169@gmail.com>
2026-06-15 09:46:51 +0500

    fix: Update server initialization to use server.listen for improved clarity

--- Phase-2/8-Social-media/backend/server.js
+++ Phase-2/8-Social-media/backend/server.js
@@ -1,10 +1,24 @@
 import app from "./app.js";
 import http from "http";
-import {initSocket} from '../backend/sockets/socket.js'
+import { initSocket } from "../backend/sockets/socket.js";
 
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app)
-initSocket(server)
-app.listen(PORT, () => {
-  console.log(`Server is Running on ${PORT}`);
+const server = http.createServer(app);
+initSocket(server);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+// import app from "./app.js";
+// import http from "http";
+// import {initSocket} from '../backend/sockets/socket.js'
+
+// const PORT = process.env.PORT || 3000;
+// const server = http.createServer(app)
+// initSocket(server)
+// app.listen(PORT, () => {
+//   console.log(`Server is Running on ${PORT}`);
+// });
